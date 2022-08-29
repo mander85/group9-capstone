71377073897fdea77ce7867ba190acfaaee8ff31
mander85 <mander85@asu.edu>
2022-08-29 12:09:37 -0400

    Started a new project for the web app capstone
    New project created with a new application
    -all major work is located in the fitness file tree
    -Mostly user interface work, templates, url paths, and nav bar completed

---
+++ capstone_project/fitness/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+#URL name changes WILL affect navbar routing
+urlpatterns = [
+    path('', views.home, name='fitness-home'),
+    path('index/', views.home, name='fitness-home'),
+    path('workouts/', views.workouts, name='fitness-workouts' ),
+    path('leaderboard/', views.leaderboard, name='fitness-leaderboard' ),
+    path('profile/', views.profile, name='fitness-profile' ),
+    path('signinup/', views.signinup, name='fitness-signinup' ),
+]
