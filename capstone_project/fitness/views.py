03cf2c7e03f67c78b57e270c18bb28933df06f7c
mander85 <mander85@asu.edu>
2022-08-29 14:05:36 -0400

    Updates to comments and database
    Database migrated and 3 admin(superuser) roles added to application
    Test user was added
    Some comments were added for clarity

--- capstone_project/fitness/views.py
+++ capstone_project/fitness/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-
+#views allow for navigation across the webpage using the templates in capstone_project/fitness/templates
 def home(request):
-    return render(request, 'fitness/index.html') # context)
+    return render(request, 'fitness/index.html') 
 
 def workouts(request):
     return render(request, 'fitness/workouts.html')
