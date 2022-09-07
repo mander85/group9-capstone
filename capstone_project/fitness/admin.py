bd710bbc3c5abe09ee784ec7dc2dc94dbf70cd57
mander85 <mander85@asu.edu>
2022-09-06 21:20:45 -0400

    Leaderboard nearly complete
    -pagination implmented
    -some css changes
    -rankings added
    -ranking saves across pages

--- capstone_project/fitness/admin.py
+++ capstone_project/fitness/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Leader_Post
+#from .models import Leader_Post
+from .models import Leader_Post_bytime
 # Register your models here.
 
-admin.site.register(Leader_Post)
+#### Defunct model kept in case of future use 
+#admin.site.register(Leader_Post)
+
+admin.site.register(Leader_Post_bytime)
--- capstone_project/fitness/views.py
+++ capstone_project/fitness/views.py
@@ -1,10 +1,11 @@
+from msilib.schema import ListView
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Leader_Post
-from .forms import LeaderboardForm
+from .models import  Leader_Post_bytime #,Leader_Post
+from .forms import LeaderboardForm_bytime #, LeaderboardForm
 from django.contrib import messages
-from django.contrib.auth.models import User
-
+#from django.contrib.auth.models import User
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
@@ -18,27 +19,33 @@ def workouts(request):
 
 @login_required
 def leaderboard(request):
-    form = LeaderboardForm
+    posts=Leader_Post_bytime.objects.order_by('-time')
+    page = request.GET.get('page',1)
+    paginator = Paginator(posts, 10)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    form = LeaderboardForm_bytime
     #handles the form submission 
     if request.method == "POST":
-        form = LeaderboardForm(request.POST)
+        form = LeaderboardForm_bytime(request.POST)
         if form.is_valid():
-            #allows the user to enter reps without having to select their username
+            #allows the user to enter time without having to select their username
             leaderpost = form.save(commit=False)
             leaderpost.submitter = request.user
             form.save()
-            messages.success(request, f'Reps submitted!')
+            messages.success(request, f'Time submitted!')
             return redirect ('fitness-leaderboard')
         else:
-            messages.warning(request, f'Please enter your reps' )
+            messages.warning(request, f'Please enter your time' )
+
     #if the user has not submitted the form
     else:
-        #provides the leaderboard data sorted from high to low by reps
-        posts=Leader_Post.objects.order_by('-reps')
-        return render(request, 'fitness/leaderboard.html', {'form': form, 'posts': posts})
+        #provides the leaderboard data sorted from high to low by time
+        
+        return render(request, 'fitness/leaderboard.html', {'posts': posts,'form': form })
 
 
-
-
-
-    
